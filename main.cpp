0e5a197168f2d52a4939aa5fcd94b0d105e8020b
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2025-03-22 14:31:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <thread>
+
+const int LIMIT = 500'000'000;
+
+void increment_without_mutex()
+{
+    int counter = 0;
+    auto start 	= std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < LIMIT; ++i)
+        ++counter;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Without mutex - Final counter: " << counter
+              << ", Time taken: " << duration.count() << " seconds\n";
+}
+
+void increment_with_mutex() {
+    int counter = 0;
+    std::mutex mtx;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < LIMIT; ++i)
+	{
+        std::lock_guard<std::mutex> lock(mtx);
+        ++counter;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "With mutex    - Final counter: " << counter
+              << ", Time taken: " << duration.count() << " seconds\n";
+}
+
+int main()
+{
+    increment_without_mutex();
+    increment_with_mutex();
+    return 0;
+}
